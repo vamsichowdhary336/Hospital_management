d4680f126d6ac84297fd91efdfce533e47aaaa4c
vamsichowdhary336 <vamsichowdhary336@gmail.com>
2026-01-03 14:37:39 +0530

    url fixed

--- src/main/java/com/project/hospitalManagementSystem/config/CorsConfig.java
+++ src/main/java/com/project/hospitalManagementSystem/config/CorsConfig.java
@@ -13,7 +13,7 @@ public class CorsConfig {
 		return new WebMvcConfigurer() {
 			@Override
 			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("/**").allowedOrigins("https://warm-tapioca-f3d47a.netlify.app/")
+				registry.addMapping("/**").allowedOrigins("https://warm-tapioca-f3d47a.netlify.app")
 						.allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS").allowedHeaders("*")
 						.allowCredentials(true);
 			}
